014c03b7073ea3c2f9ee7375c65d603179abe4fe
Noah <noahprince8@gmail.com>
2014-01-16 16:38:15 -0600

    added unit tests

---
+++ test/unit/basic.rb
@@ -0,0 +1,18 @@
+class Bowling
+  def hit(pins)
+  end
+
+  def score
+    0
+  end
+end
+
+
+
+describe Bowling, "#score" do
+  it "returns 0 for all gutter game" do
+    bowling = Bowling.new
+    20.times { bowling.hit(0) }
+    bowling.score.should eq(1)
+  end
+end
