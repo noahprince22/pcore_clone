c95c0cbb07eb54191f6bf3bcf3b7ffef3ef36190
Noah <noahprince8@gmail.com>
2014-03-27 20:39:11 -0500

    failed

--- features/google_steps.rb
+++ features/google_steps.rb
@@ -2,6 +2,7 @@ require 'bundler'
 Bundler.require(:test)
 
 Given(/^I should be able to search$/) do
+  raise "FAILLLLL BITCH"
   @driver.send_keys(:name,"q","google")
   # @driver.wait_and_click(:name,"btnK")
   @driver.wait_to_appear(:id,"extabar")
